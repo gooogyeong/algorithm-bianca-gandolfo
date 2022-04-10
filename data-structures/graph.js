85a47d72ea2941154c375fa13776dfb1b4a97270
Minkyung Lee <gooogyeong@gmail.com>
2022-04-10 12:54:28 +0900

    feature: graph methods implementation

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -86,52 +86,166 @@ Given a directed graph and two nodes in the graph, write a function that indicat
 
 */
 
-
 function Graph () {
-  this._nodes = {};
+  this._nodes = {}
 }
 
-Graph.prototype.addNode = function(value) {
-  // implement me...
-};
+Graph.prototype.addNode = function (value) {
+  if (!value) throw new Error('Value is undefined')
+  this._nodes[value] = this._nodes[value] || []
+}
 // Time complexity:
 
-Graph.prototype.removeNode = function(value) {
-  // implement me...
-};
+Graph.prototype.removeNode = function (value) {
+  this._nodes[value].forEach(node => {
+    const neighbors = this._nodes[node]
+    const deleteNodeIdx = neighbors.indexOf(value)
+    if (deleteNodeIdx > -1) neighbors.splice(deleteNodeIdx, 1)
+  })
+  delete this._nodes[value]
+}
 // Time complexity:
 
-Graph.prototype.contains = function(value) {
-  // implement me...
-};
+Graph.prototype.contains = function (value) {
+  return !!this._nodes[value]
+}
 // Time complexity:
 
-Graph.prototype.addEdge = function(value1, value2) {
-  // implement me...
-};
+Graph.prototype.addEdge = function (value1, value2) {
+  if (!this._nodes[value1] || !this._nodes[value2]) {
+    throw new Error('Cannot find node to add edge to')
+  }
+  this._nodes[value1].push(value2)
+  this._nodes[value2].push(value1)
+}
+
 // Time complexity:
 
-Graph.prototype.removeEdge = function(value1, value2) {
-  // implement me...
-};
+Graph.prototype.removeEdge = function (value1, value2) {
+  if (!this._nodes[value1] || !this._nodes[value2]) {
+    throw new Error('Cannot find node to add edge to')
+  }
+  this._nodes[value1].splice(this._nodes[value1].indexOf(value2), 1)
+  this._nodes[value2].splice(this._nodes[value2].indexOf(value1), 1)
+}
 // Time complexity:
 
-Graph.prototype.hasEdge = function(value1, value2) {
-  // implement me...
-};
+Graph.prototype.hasEdge = function (value1, value2) {
+  return this._nodes[value1].indexOf(value2) > -1 || this._nodes[value2].indexOf(value2) > -1
+}
 // Time complexity:
 
-Graph.prototype.forEach = function(fn) {
-  // implement me...
-};
+Graph.prototype.forEach = function (fn) {
+  for (let node in this._nodes) {
+    fn(node, this._nodes[node], this._nodes)
+  }
+}
 // Time complexity:
 
-Graph.prototype.traverseDepthFirst = function(value, fn, visited, distance) {
-  // implement me...
-};
-// Time complexity:
+Graph.prototype.traverseDepthFirst = function (value, fn, visited = {}, distance = 0) {
+  visited[value] = true
+  fn(value, distance)
+  this._nodes[value].forEach(function (neighbor) {
+    if (visited[neighbor]) return
+    this.traverseDepthFirst(neighbor, fn, visited, distance + 1)
+  }, this)
+}
 
-Graph.prototype.traverseBreadthFirst = function(value, fn) {
-  // implement me...
-};
-// Time complexity:
+Graph.prototype.traverseBreadthFirst = function (value, fn) {
+  const distance = {}
+  distance[value] = 0
+  let queue = [value]
+  while (queue.length) {
+    const node = queue.shift()
+    fn(node, distance[node])
+    const neighbors = this._nodes[node].filter(n => {
+      if (distance[n] === undefined) {
+        distance[n] = distance[node] + 1
+        return true
+      }
+    })
+    queue = [...queue, ...neighbors]
+  }
+}
+
+const graph = new Graph()
+
+graph.addNode(1)
+graph.addNode(2)
+graph.addNode(3)
+graph.addNode(4)
+graph.addNode(5)
+console.log(graph._nodes, 'should have 5') // { '1': [], '2': [], '3': [], '4': [], '5': [] } should have 5
+graph.removeNode(5)
+console.log(graph._nodes, 'should NOT have 5') // { '1': [], '2': [], '3': [], '4': [] } should NOT have 5
+console.log(graph.contains(4), 'should be true') // true
+console.log(graph.contains(7), 'should be false') // false
+graph.addEdge(1, 2)
+graph.addEdge(1, 4)
+graph.addEdge(3, 2)
+graph.addEdge(2, 4)
+graph.addEdge(3, 4)
+console.log(graph._nodes)
+// // { 1: [ 2, 4 ],
+// //   2: [ 1, 3, 4 ],
+// //   3: [ 2, 4 ],
+// //   4: [ 1, 2, 3 ]
+// // }
+graph.removeEdge(4, 3)
+console.log(graph._nodes)
+// //   1: [ 2, 4 ],
+// //   2: [ 1, 3, 4 ],
+// //   3: [ 2 ],
+// //   4: [ 1, 2 ]
+// // }
+console.log(graph.hasEdge(1, 2), 'should be true') // true
+console.log(graph.hasEdge(1, 3), 'should be false') // false
+graph.forEach(function (node, neighbors) {
+  console.log(node, 'has neighbors:', neighbors)
+})
+// // 1 has neighbors: [ 2, 4 ]
+// // 2 has neighbors: [ 1, 3, 4 ]
+// // 3 has neighbors: [ 2 ]
+// // 4 has neighbors: [ 1, 2 ]
+graph.addNode(5)
+graph.addEdge(3, 5)
+// console.log(graph._nodes)
+// // {
+// //   '1': [ 2, 4 ],
+// //   '2': [ 1, 3, 4 ],
+// //   '3': [ 2, 5 ],
+// //   '4': [ 1, 2 ],
+// //   '5': [ 3 ]
+// // }
+// graph.removeNode(4)
+// console.log(graph._nodes)
+/*{
+  1: [ 2 ],
+  2: [ 1, 3 ],
+  3: [ 2, 5 ],
+  5: [ 3 ]
+}*/
+console.log('=== traverse ===')
+console.log(graph)
+/*
+Graph {
+  _nodes: {
+    '1': [ 2, 4 ],
+    '2': [ 1, 3, 4 ],
+    '3': [ 2, 5 ],
+    '4': [ 1, 2 ],
+    '5': [ 3 ]
+  }
+}
+*/
+
+const traverseDF = []
+graph.traverseDepthFirst(1, (val, dist) => {
+  traverseDF.push([val, dist])
+})
+console.log(traverseDF, '\nshould be [[ 1, 0 ], [ 2, 1 ], [ 3, 2 ], [ 5, 3 ], [ 4, 2 ]]')
+const traverseBF = []
+graph.traverseBreadthFirst(1, (val, dist) => {
+  traverseBF.push([val, dist])
+})
+console.log(traverseBF, '\nshould be [[ 1, 0 ], [ 2, 1 ], [ 4, 1 ], [ 3, 2 ], [ 5, 3 ]]')
